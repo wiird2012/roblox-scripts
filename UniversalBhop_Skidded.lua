8ffe428adf5b833ae5433e189b9d15ffffed3437
wiird2012 <66582105+wiird2012@users.noreply.github.com>
2023-10-27 01:56:20 -0300

    Update UniversalBhop_Skidded.lua

--- UniversalBhop_Skidded.lua
+++ UniversalBhop_Skidded.lua
@@ -1,63 +1,59 @@
-local UIS = game:GetService("UserInputService")
-
-local bhopinfo = {
-    CurrentVel = 0, -- DONT TOUCH
-    VelCap = 3,
-    JumpBoostAmt = 0.1
-    local evpatch = true
+getgenv().bhopinfo = {
+  CurrentVel = 0, -- DONT TOUCH
+  VelCap = 3,
+  JumpBoostAmt=3/5,
+  groundamt=4,
+  RolvePatch=false, -- enables autojumping
+  JPatch=false, -- if game somehow does not fire JumpRequest causing it to break
+  evpatch=true, --stops disabling jumping
 }
-
-local RolvePatch = false -- CHANGE false to true IF YOU WANT TO USE THIS ON ROLVE GAMES (CB:RO ARSENAL)
+if not funcs then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/exceptional0/scr/main/funcs.lua"))()
+end
 
 if bhopinfo.evpatch then
-
 getgenv().bhopinfo.jp=getchar():WaitForChild("Humanoid",10).JumpPower
 getgenv().bhopinfo.jh=getchar():WaitForChild("Humanoid",10).JumpHeight
 end
 getgenv().bhopinfo.JumpBoostAmt=getgenv().bhopinfo.VelCap/5
 
-local helper = {
-    getChar = function()
-        return game.Players.LocalPlayer.Character
-    end
-}
+local function checkOnGround(char,setg)
+  local ray = Ray.new(char:WaitForChild("HumanoidRootPart",5).Position,-(char:WaitForChild("HumanoidRootPart",5).CFrame.UpVector * 100))
+  local part, pos = workspace:FindPartOnRay(ray,char)
+ 
+  if part then
+      if pos then
+          local farness = math.ceil((char:WaitForChild("HumanoidRootPart",10).Position - pos).Magnitude)
+          if setg then        
+  return farness        
+  end
+         
+if farness > getgenv().bhopinfo.groundamt then    -- works with normal blocky r15 avatars.. sooo :l
+              --print(farness,"False")
+return false
+          elseif farness <= getgenv().bhopinfo.groundamt then
+            --print(farness,"true")    
+return true
+          end
+      end
+  end
+end
 
-local function checkOnGround(char)
-    local ray = Ray.new(char.HumanoidRootPart.Position,-(char.HumanoidRootPart.CFrame.UpVector * 100))
-    local part, pos = workspace:FindPartOnRay(ray,char)
-    
-    if part then
-        if pos then
-            local farness = math.ceil((char.HumanoidRootPart.Position - pos).Magnitude)
-            if farness > 3 then    -- works with normal blocky r15 avatars.. sooo :l
-                return false
-            elseif farness <= 3 then
-                return true
-            end
-        end
-    end
+
+getgenv().bhopinfo.setg=function()
+getgenv().bhopinfo.groundamt=checkOnGround(getchar(),true)
 end
 
 
 
-UIS.JumpRequest:Connect(function()
+funcs.uip.JumpRequest:Connect(function()
 
-    if (UIS:IsKeyDown(Enum.KeyCode.W) == false) and (UIS:IsKeyDown(Enum.KeyCode.A) or UIS:IsKeyDown(Enum.KeyCode.D)) == true and BhopInfo.CurrentVel < BhopInfo.VelCap then
-        BhopInfo.CurrentVel = BhopInfo.CurrentVel + BhopInfo.JumpBoostAmt
+if getgenv().bhopinfo.CurrentVel < getgenv().bhopinfo.VelCap then
+    getgenv().bhopinfo.CurrentVel = getgenv().bhopinfo.CurrentVel + getgenv().bhopinfo.JumpBoostAmt
     end
+  --print("jreq")
 end)
 
--- patchs games like restrict jumping honestly really any other game --
-
-local char = helper.getChar()
-
-char.Humanoid.StateChanged:Connect(function(oldstate,newstate)
-    if newstate == Enum.HumanoidStateType.Landed then -- if we've landed after we've jumped then allow us to jump again, essentially breaking the jump cooldown
-        char.Humanoid:SetStateEnabled(Enum.HumanoidStateType.Jumping,true)
-
-        print("resetting jump cd")
-    end
-end)
 
 -- patchs games like restrict jumping honestly really any other game --
 local hum
@@ -114,24 +110,43 @@ end))
 end
 
 ----------------------------------------
+if bhopinfo.JPatch then
+funcs.uip.InputBegan:Connect(function(inp)
+if inp.UserInputType == Enum.UserInputType.Keyboard then
+if inp.KeyCode == Enum.KeyCode.Space then
+while funcs.uip:IsKeyDown(Enum.KeyCode.Space) do
+if not getchar():WaitForChild("Humanoid",10).Jump then
+firesignal(funcs.uip.JumpRequest)
+end
+task.wait(.0033333333333333)
+end
 
-spawn(function() -- spawn, wait, delay, and Debris are ALL SO BAD. NEVER RELY ON THEM. I am relying on them though because this is a exploit script, not much effort put into it
-    while true do
-        local char = helper.getChar()
-    
-        if checkOnGround(char) == false and BhopInfo.CurrentVel ~= 0 then
-            char.HumanoidRootPart.CFrame = char.HumanoidRootPart.CFrame + (char.HumanoidRootPart.CFrame.LookVector * BhopInfo.CurrentVel/6)
-            print('hopping')
-        end
-
-        if UIS:IsKeyDown(Enum.KeyCode.Space) == false then
-            BhopInfo.CurrentVel = 0
-        elseif UIS:IsKeyDown(Enum.KeyCode.Space) == true and UIS:IsKeyDown(Enum.KeyCode.W) then
-            BhopInfo.CurrentVel = math.clamp(BhopInfo.CurrentVel - 0.01,0,BhopInfo.VelCap)
-        elseif UIS:IsKeyDown(Enum.KeyCode.Space) == true then
-            if RolvePatch == true then char.Humanoid.Jump = true end -- patches rolve games
-        end
-
-        game:GetService("RunService").Stepped:Wait()
-    end
+end
+end
 end)
+
+end
+local function theroundabout()
+
+      if checkOnGround(getchar()) == false and getgenv().bhopinfo.CurrentVel ~= 0 then
+          getchar():PivotTo(getchar():GetPivot() + (getchar():GetPivot().LookVector * getgenv().bhopinfo.CurrentVel/6))
+          --print('hopping')
+else
+--print(funcs.uip:IsKeyDown(Enum.KeyCode.Space),funcs.uip:IsKeyDown(Enum.KeyCode.W),getgenv().bhopinfo.CurrentVel,checkOnGround(getchar()) )
+      end
+
+      if funcs.uip:IsKeyDown(Enum.KeyCode.Space) == false then
+          getgenv().bhopinfo.CurrentVel = 0
+     -- elseif funcs.uip:IsKeyDown(Enum.KeyCode.Space) == true and funcs.uip:IsKeyDown(Enum.KeyCode.W) then
+     --    getgenv().bhopinfo.CurrentVel = math.clamp(getgenv().bhopinfo.CurrentVel - 0.01,0,getgenv().bhopinfo.VelCap)
+      elseif bhopinfo.RolvePatch then
+       getchar():WaitForChild("Humanoid",10).Jump = true -- patches rolve games
+      end
+end
+task.spawn(function() -- spawn, wait, delay, and Debris are ALL SO BAD. NEVER RELY ON THEM. I am relying on them though because this is a exploit script, not much effort put into it
+  while true do
+  pcall(theroundabout)
+      task.wait()
+  end
+end)
+bhopinfo.setg()
