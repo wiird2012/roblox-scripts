17c45d5aefac060e6b7761e3d49c25a09aa180f4
Lilith <66582105+wiird2012@users.noreply.github.com>
2025-12-30 21:32:13 -0300

    Update desertbus_autoevent.lua

--- desertbus_autoevent.lua
+++ desertbus_autoevent.lua
@@ -1,4 +1,4 @@
-_G.jedalfarm = true --Switch to false if you want to stop the script
+_G.jedalfarm = true -- Switch to false if you want to stop the script
 
 --[[
 HOW TO USE:
@@ -9,13 +9,13 @@ Idling near the 5 gifts in the second floor should get you the most
 You need a total of 640 Jedals to complete the event)
 Just to be safe use in a private server!!
 
-Made with love, by Lilith // wiird2012
+Made with ❤, by Lilith // wiird2012
 ]]
 
 while _G.jedalfarm == true do
-    task.wait(.5) --Increase the delay if this lags for you
+    task.wait(.5) -- Increase the delay if this lags for you
         for i,v in pairs(workspace.Decorations.Jedals.Gifts["Script"]:GetChildren()) do
-            if v.Name == "Base" then
+            if v.Name == "Base" then -- Checks if model name is Base, otherwise it errors as soon it runs because of a new model named "Model". Crazy
             fireclickdetector(v.Click.ClickDetector)
         end
     end
