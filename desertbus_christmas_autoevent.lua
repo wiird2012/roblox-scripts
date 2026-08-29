8128b4255ae4820065755194428d1c3cb1920e81
Lilith <66582105+the-lith@users.noreply.github.com>
2026-08-29 13:26:03 -0300

    Update and rename desertbus_autoevent.lua to desertbus_christmas_autoevent.lua

--- desertbus_christmas_autoevent.lua
+++ desertbus_christmas_autoevent.lua
@@ -3,13 +3,11 @@ _G.jedalfarm = true -- Switch to false if you want to stop the script
 --[[
 HOW TO USE:
 Load in to Desert Bus and execute the script.
-It should start working as long you get near the gift boxes
+It should start working as long you get close to the gift boxes
 Idling near the 5 gifts in the second floor should get you the most
 
 You need a total of 640 Jedals to complete the event
-Just to be safe use in a private server!!
-
-Made with ❤, by Lilith // wiird2012
+Just to be safe use in a private server
 ]]
 
 while _G.jedalfarm == true do
